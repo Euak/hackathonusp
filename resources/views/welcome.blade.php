88288313140e3c1f7de4881c05a2ab1fa3eda847
Kaue Oliveira Almeida <ueak21@gmail.com>
2018-11-10 23:42:41 -0200

    unidades list

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,42 +17,83 @@
     </head>
     <body>
         <div class="container-fluid">
-          <!-- Header -->
-          <div class="row">
-            <div class="col">
-              <nav class="navbar navbar-expand-sm bg-light justify-content-center">
-                <!-- Links -->
-                <ul class="navbar-nav">
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Sobre</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Tabelas</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Gráficos</a>
-                  </li>
-                </ul>
-              </nav>
+            <!-- Header -->
+            <div class="row bg-secondary">
+                <div class="col">
+                    <nav class="navbar navbar-expand-sm justify-content-center">
+                        <!-- Links -->
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Sobre</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Tabelas</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Gráficos</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
-          </div>
-          <!-- Content -->
-          <div class="row">
-            <div class="col bg-gray">
+            <!-- Content -->
+            <div class="row" id="big_numbers">
+                <div class="col">
                 <div class="row">
                   <div class="col text-center">
-                    <h2 class="display-1" id="num_li_aberta">1324</h2>
+                    <h2 class="display-1 mb-0" id="num_li_aberta">643</h2>
                     <h3>Licitações abertas</h3>
                   </div>
                 </div>
                 <div class="row">
                   <div class="col text-center">
-                    <h2 class="display-1" id="num_li_enc">643</h2>
+                    <h2 class="display-1 mb-0" id="num_li_enc">1324</h2>
                     <h3>Licitações encerradas</h3>
                   </div>
                 </div>
             </div>
-          </div>
+            </div>
+
+            <div class="row mt-5">
+                <div class="col">
+                    <div class="row">
+                  <div class="col-md-4 text-center">
+                      <img src="/img/unidades/1.jpeg" alt="">
+                  </div>
+                  <div class="col-md-4 text-center">
+                      <img src="/img/unidades/1.jpeg" alt="">
+                  </div>
+                  <div class="col-md-4 text-center">
+                      <img src="/img/unidades/1.jpeg" alt="">
+                  </div>
+              </div>
+                    <div class="row mt-4">
+                  <div class="col-md-4 text-center">
+                    <img src="/img/unidades/1.jpeg" alt="">
+                  </div>
+                  <div class="col-md-4 text-center">
+                    <img src="/img/unidades/1.jpeg" alt="">
+                  </div>
+                  <div class="col-md-4 text-center">
+                    <img src="/img/unidades/1.jpeg" alt="">
+                  </div>
+              </div>
+                    <div class="row mt-4">
+                  <div class="col-md-4 text-center">
+                      <img src="/img/unidades/1.jpeg" alt="">
+                  </div>
+                  <div class="col-md-4 text-center">
+                      <img src="/img/unidades/1.jpeg" alt="">
+                  </div>
+                  <div class="col-md-4 text-center">
+                      <img src="/img/unidades/1.jpeg" alt="">
+                  </div>
+              </div>
+                </div>
+            </div>
+
+
+
           <!-- Footer -->
           <div class="row">
             <div class="col">
@@ -61,8 +102,8 @@
         </div>
     </body>
     <script>
-        var numAnim = new CountUp("num_li_aberta", 0, 1324);
-        var numAnim2 = new CountUp("num_li_enc", 0, 643);
+        var numAnim = new CountUp("num_li_aberta", 0, 643);
+        var numAnim2 = new CountUp("num_li_enc", 0, 1324);
         if (!numAnim.error) {
             numAnim.start();
             numAnim2.start();
